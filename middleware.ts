6affa71989f12fda8d38dcd4c8accc4366806b74
Tiago <beckertiago09@gmail.com>
2023-02-14 17:33:21 -0300

    layout dash

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ import { NextResponse } from "next/server";
      const token  = req.cookies.get("blogCookie")
 
     if(!token && req.url.includes("/Admin")){
-        return NextResponse.redirect("https://apiblog-production.up.railway.app/signin")
+        return NextResponse.redirect("https://blog-uuxg.vercel.app/signin")
     }
     if(token && req.url === "https://apiblog-production.up.railway.app/signin"){
-         return NextResponse.redirect("https://apiblog-production.up.railway.app/Admin")
+         return NextResponse.redirect("https://blog-uuxg.vercel.app/Admin")
     }
    
 
